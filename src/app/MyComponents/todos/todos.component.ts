70bcf4d0b2cb5e149edcf12f22d6d813965aadb1
SujendraNayak <sujinayak008@gmail.com>
2025-01-24 21:06:31 +0530

    component update

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Todo} from '../../Todo'
 @Component({
   selector: 'app-todos',
   imports: [],
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
+  todos: Todo[];
+  constructor() { 
+    this.todos=[
+      {
+        sno:1,
+        title:"this title",
+        desc:"Descrption",
+        active:true
+      },
+      {
+        sno:2,
+        title:"this title",
+        desc:"Descrption",
+        active:true
+      },
+      {
+        sno:3,
+        title:"this tile",
+        desc:"Descrption",
+        active:true
+      }
+    ]
+  }
 
 }
